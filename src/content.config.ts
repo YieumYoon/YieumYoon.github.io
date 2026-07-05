5d935e222bad8f01b01f4e6241129875301e7e9c
Yieum Yoon <junsulee.tech@gmail.com>
2026-07-05 17:23:09 -0400

    minor changes

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,6 +8,8 @@ const blog = defineCollection({
     title: z.string(),
     summary: z.string(),
     date: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    image: z.string().optional(),
     tags: z.array(z.string()),
     draft: z.boolean().optional(),
   }),
